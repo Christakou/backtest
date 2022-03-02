ae0b9a564ddb8321d47ed22629e6e63312376588
Christakou <christiano.christakou@gmail.com>
2022-03-02 01:11:03 +0000

    feat: Adding tests and started work on market data client

--- backtest_module/marketdata.py
+++ backtest_module/marketdata.py
@@ -1,7 +1,11 @@
-from tokenize import String
+from functools import cache
 import backtest_module.backtest_config as backtest_config
 import requests
+import json
 
+
+class APIError(Exception):
+    pass
 
 
 class MDClient:
@@ -14,9 +18,24 @@ class MarketStackBackEnd:
     def __init__(self) -> None:
         self.session = requests.session()
         self.api_key = self._read_secrets()
+        self.base_url = 'http://api.marketstack.com/v1'
 
-        
-    def _read_secrets(self) -> String:
+    def _read_secrets(self) -> str:
         with open(backtest_config.API_KEY_PATH) as file:
             api_key = file.read()
         return api_key
+
+    def _eod_data(self, symbol_list, start_date=None, end_date=None, limit=100, offset=0):
+        params = {
+            'access_key': self.api_key,
+            'symbols': ','.join(symbol_list),
+            'date_from': start_date,
+            'date_to': end_date,
+            'limit': limit,
+            'offset': offset,
+        }
+        response = self.session.get(f'{self.base_url}/eod/latest', params=params)
+        if response.ok:
+            return json.loads(response.content)
+        else:
+            raise APIError(f'{response.status_code} ERROR: {response.content}')
--- tests/test_marketdata.py
+++ tests/test_marketdata.py
@@ -1,7 +1,24 @@
-from backtest_module.marketdata import MarketStackBackEnd
+from backtest_module.marketdata import MarketStackBackEnd, APIError
+import pytest
+
+@pytest.fixture()
+def test_backend():
+    test_backend = MarketStackBackEnd()
+    return test_backend
 
 def test_market_stack_back_end_reads_secrets():
     test_backend = MarketStackBackEnd()
     print(test_backend.api_key)
     assert len(test_backend.api_key) != 0
+
+
+def test_eod_data_fetches_data_from_endpoint(test_backend):
+    symbols = ['AAPL']
+    assert len(test_backend._eod_data(symbols,'2021-02-03')) != 0
+
+def test_eod_data_raises_invalid_symbol(test_backend):
+    """ Test that the _eod function raises an error if no valid symbols are passed"""
+    symbols = ['gamestonk']
+    with pytest.raises(APIError):
+        test_backend._eod_data(symbols, '2021-02-03')
 
