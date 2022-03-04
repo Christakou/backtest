d6ceb24f7aba31a1d98879d27cc544691ba6f9d7
Christakou <christiano.christakou@gmail.com>
2022-03-04 21:42:09 +0000

    feat: Implemented more functionality into marketdata.py and tests

--- backtest_module/marketdata.py
+++ backtest_module/marketdata.py
@@ -2,15 +2,11 @@ from functools import cache
 import backtest_module.backtest_config as backtest_config
 import requests
 import json
-
+import pandas as pd
 
 class APIError(Exception):
     pass
 
-
-class MDClient:
-    def __init__(self, backend) -> None:
-        pass
 
 
 class MarketStackBackEnd:
@@ -25,17 +21,65 @@ class MarketStackBackEnd:
             api_key = file.read()
         return api_key
 
-    def _eod_data(self, symbol_list, start_date=None, end_date=None, limit=100, offset=0):
+    def get_market_data(self, symbols, start_date=None, end_date=None):
+        df = self._parse(self._eod_data(symbols, start_date, end_date))
+        df['date'] = pd.to_datetime(df['date'])
+        return df
+
+    @cache
+    def _eod_data(self, symbols, start_date=None, end_date=None, limit=1000, offset=0):
+        if type(symbols) == tuple:
+            symbols = ','.join(symbols)
+        else:
+            symbols = symbols
         params = {
             'access_key': self.api_key,
-            'symbols': ','.join(symbol_list),
+            'symbols': symbols,
             'date_from': start_date,
             'date_to': end_date,
             'limit': limit,
             'offset': offset,
         }
-        response = self.session.get(f'{self.base_url}/eod/latest', params=params)
+        response = self.session.get(f'{self.base_url}/eod', params=params)
         if response.ok:
             return json.loads(response.content)
         else:
             raise APIError(f'{response.status_code} ERROR: {response.content}')
+
+    def _parse(self, response_content):
+        df = pd.DataFrame(response_content['data'])
+        return df
+
+
+class MarketData:
+    def __init__(self, symbols, start_date='2021-01-01', end_date='2022-01-01', backend=MarketStackBackEnd()):
+        self.data = backend.get_market_data(symbols,start_date,end_date)
+        self.start_date = start_date
+        self.end_date = end_date
+        self.instruments = {}
+        self.get_instruments()
+        self.name = f'MarketData-{start_date} - {end_date} - {",".join(self.instruments)}'
+    def __str__(self):
+        return self.name
+
+    def close_at(self,symbol,date):
+        return self.data[(self.data['symbol']==symbol) & (self.data['date']==date)]['close'].values[0]
+
+
+    def get_instruments(self):
+        for symbol in self.data['symbol'].unique():
+            self.instruments.update({symbol:Instrument(symbol,self)})
+
+    def get_instrument_prices(self, symbol):
+        return self.data[(self.data['symbol']==symbol)]['close']
+
+
+class Instrument():
+    def __init__(self, symbol, marketdata):
+        self.symbol = symbol
+        self.prices = marketdata.get_instrument_prices(self.symbol)
+
+    def __str__(self):
+        return f'Instrument({self.symbol})'
+    def __repr__(self):
+        return self.__str__()
