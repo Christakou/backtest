e3764b096957ca7dd87cc20228b91ae6ebd8aaff
Christakou <christiano.christakou@gmail.com>
2022-03-07 20:36:14 +0000

    feat: Added more features

--- backtest/main.py
+++ backtest/main.py
@@ -17,10 +17,11 @@ if __name__ == '__main__':
     x1, y1 = a._plot_PGV()
     x2, y2 = b._plot_PGV()
 
-    plt.plot(x1,y1)
-    plt.plot(x2,y2)
+    plt.plot(x1,y1, label=a.name)
+    plt.plot(x2,y2, label=b.name)
     plt.title(f'PGV over time')
     plt.xticks(rotation=90)
     plt.xlabel('Date')
     plt.ylabel('PGV')
+    plt.legend()
     plt.savefig(f'Comparative Analysis of {a.name} and {b.name}.jpg', bbox_inches='tight')
